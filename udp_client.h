531f120fa01326e2eb481a79c76e52fe65085dbc
MertAdak <mert.adak@ozu.edu.tr>
2022-06-29 10:51:54 +0300

    all in one commit

---
+++ udp_client.h
@@ -0,0 +1,24 @@
+#ifndef __UDPCLIENT__
+#define __UDPCLIENT__
+
+#include <iostream>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+class udp_client{
+public:
+    int sockfd;
+    char client_message[256];
+    struct sockaddr_in serv_addr;//server address and client address
+    socklen_t sLen = sizeof(serv_addr);
+
+    udp_client(int port, char *message, const char* ip);
+    void send_message();
+};
+#endif
